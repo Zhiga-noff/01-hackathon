ad52fc4a4c0c7cc02abbce1e9b6ebef22337b68b
Ilya Petrenko <ilya.petrenko.96@mail.ru>
2023-05-12 23:32:42 +0300

    change color

--- src/app.js
+++ src/app.js
@@ -1,2 +1,13 @@
 import './styles.css'
 
+document.body.addEventListener('click', ()=>{
+	document.body.style.backgroundColor = getRandomColor();
+	function getRandomColor() {
+		const letters = '0123456789ABCDEF';
+		let color = '#';
+		for (let i = 0; i < 6; i++) {
+		  color += letters[Math.floor(Math.random() * 16)];
+		}
+		return color;
+	 }
+})
