62ae328ff0ec1f053daa8b435f7d36a0a041a048
Kirill Zhiganov <zhiganov_k.n@mail.ru>
2023-05-13 11:41:25 +0300

    Реализовал появление меню в рандомном месте. И рандомный тег li помещаю в меню

--- src/app.js
+++ src/app.js
@@ -1,2 +1,11 @@
-import './styles.css'
+import "./styles.css";
+import { ContextMenu } from "./menu";
 
+const contextMenu = new ContextMenu("#menu");
+
+document.addEventListener("contextmenu", (event) => {
+  event.preventDefault();
+  const { clientX, clientY } = event;
+  console.log(clientX, clientY);
+  contextMenu.open(clientX, clientY);
+});
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,25 @@
-import {Menu} from './core/menu'
+import { Menu } from "./core/menu";
 
 export class ContextMenu extends Menu {
+  constructor(selector) {
+    super(selector);
+  }
 
+  open(positionX, positionY) {
+    console.log(this.el);
+    this.el.classList.add("open");
+    this.el.style.top = `${positionY}px`;
+    this.el.style.left = `${positionX}px`;
+    const li = document.createElement("li");
+    li.textContent = "khdgadshgalkj";
+    this.el.append(li);
+  }
+
+  close() {
+    super.close();
+  }
+
+  add() {
+    super.add();
+  }
 }
