eb73c23bc5086d8d48251b7908dc33a2c5dec6bb
Kirill Zhiganov <zhiganov_k.n@mail.ru>
2023-05-13 18:38:47 +0300

    Добавил создание поп апа с текстом методом createModalWindow

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -11,7 +11,9 @@ export class ClicksModule extends Module {
       countClick++;
     });
     setTimeout(() => {
-      alert(`Ну ты и накликал ${countClick-1} раз(а)`);
+
+      this.createModalWindow(countClick)
+      // alert(`Ну ты и накликал ${countClick-1} раз(а)`);
       document.removeEventListener('click', () => {
         countClick++;
       });
@@ -20,5 +22,21 @@ export class ClicksModule extends Module {
 
   toHTML() {
     return super.toHTML();
+  }
+
+  createModalWindow(count) {
+    const modalWindow = document.createElement('div')
+    modalWindow.className = 'modal-window'
+
+    const closeWindow = document.createElement('div')
+    closeWindow.className = 'close-window'
+    modalWindow.append(closeWindow)
+
+    const infoAboutClick = document.createElement('p')
+    infoAboutClick.className = 'count-click'
+    infoAboutClick.textContent = `Ну ты и накликал конечно, ${count-1} раз(а)`
+    modalWindow.append(infoAboutClick)
+
+    document.body.append(modalWindow)
   }
 }
