aae36a6bf05e22c8527e2de6568ebd7719b54ae2
Ilya Petrenko <ilya.petrenko.96@mail.ru>
2023-05-13 11:19:36 +0300

    background module

--- src/app.js
+++ src/app.js
@@ -1,13 +1,5 @@
 import './styles.css'
+import { BackgroundModule } from '../src/modules/background.module.js'
 
-document.body.addEventListener('click', ()=>{
-	document.body.style.backgroundColor = getRandomColor();
-	function getRandomColor() {
-		const letters = '0123456789ABCDEF';
-		let color = '#';
-		for (let i = 0; i < 6; i++) {
-		  color += letters[Math.floor(Math.random() * 16)];
-		}
-		return color;
-	 }
-})
+const backgroundColor = new BackgroundModule('color','colors');
+backgroundColor.changeColor();
