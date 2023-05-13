25a2b215a67840ca58edaa91c3c8a0c66ff9d3c8
Dmitry Mamchenko <dmitry.mamchenko@gmail.com>
2023-05-13 15:03:48 +0300

    Добавлен метод add

--- src/menu.js
+++ src/menu.js
@@ -1,6 +1,17 @@
 import { Menu } from "./core/menu";
+import { BackgroundModule } from '../src/modules/background.module.js';
+import { FigureModule } from '../src/modules/figure.module';
+import { ClicksModule } from '../src/modules/clicks.module';
+
+const moduleFeatches = [
+  new ClicksModule('clicks', 'Считать клики (за 3 секунды)'),
+  new BackgroundModule('background', 'Поменять цвет'),
+  new FigureModule('figure', 'Создать фигуру'),
+];
 
 export class ContextMenu extends Menu {
+  #moduleName
+
   constructor(selector) {
     super(selector);
   }
@@ -12,21 +23,9 @@ export class ContextMenu extends Menu {
     const windowWidth = document.documentElement.clientWidth;
     const windowHeight = document.documentElement.clientHeight;
 
-    this.add();
-
-    const li = document.createElement("li");
-		li.classList = 'menu-item';
-		li.textContent = "Считать клики (за 3 секунды)";
-		const li2 = document.createElement("li");
-		li2.classList = 'menu-item';
-		li2.textContent = "Создать фигуру";
-		const li3 = document.createElement("li");
-		li3.classList = 'menu-item';
-		li3.textContent = "Поменять цвет";
-		const li4 = document.createElement("li");
-		li4.classList = 'menu-item';
-		li4.textContent = "Вызвать сообщение";
-		this.el.append(li,li2,li3,li4);
+    moduleFeatches.forEach(module => {
+      this.add(module);
+    });
 
     if (positionX + this.el.offsetWidth >= windowWidth) {
       this.el.style.left = `${positionX - this.el.offsetWidth}px`;
@@ -44,10 +43,20 @@ export class ContextMenu extends Menu {
   close() {
 	console.log('close');
 	this.el.classList.remove('open');
-	document.querySelectorAll('li').forEach(elem => elem.remove());
+	  document.querySelectorAll('li').forEach(elem => elem.remove());
   }
 
-  add() {
-    super.add();
+  add(module) {
+    const moduleHTML = module.toHTML(); //так как модуль возвращает HTML, создаем ноду
+    this.el.insertAdjacentHTML('beforeend', moduleHTML); //добавление в пунт меню.
+    let moduleNode = this.el.querySelector(`[data-type=${module.type}`);
+    
+    if (typeof moduleNode !== null) { //обработка клика
+      moduleNode.addEventListener('click', () => {
+        const nameModule = moduleNode.dataset.type;
+        const activeModule = moduleFeatches.find(modul => modul.type === nameModule);
+        activeModule.trigger();
+      });
+    }
   }
 }
