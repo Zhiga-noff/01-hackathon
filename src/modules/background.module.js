c8617c6d01315e2a9e8876b67ae102e8fc27019d
Kirill Zhiganov <zhiganov_k.n@mail.ru>
2023-05-13 17:41:17 +0300

    нужно поправить фичи

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,20 @@
 import {Module} from '../core/module'
 
 export class BackgroundModule extends Module {
-
+	constructor(type,text){
+		super(type,text);
+	}
+	changeColor(){
+		document.body.addEventListener('click', ()=>{
+			document.body.style.backgroundColor = getRandomColor();
+			function getRandomColor() {
+				const letters = '0123456789ABCDEF';
+				let color = '#';
+				for (let i = 0; i < 6; i++) {
+				  color += letters[Math.floor(Math.random() * 16)];
+				}
+				return color;
+			}
+		})
+	}
 }
