345583a70c5734cae315c18cf67729c357ce4537
Kirill Zhiganov <zhiganov_k.n@mail.ru>
2023-05-13 11:07:59 +0300

    Сделал не проверенный на работе обработчик кликов. Доработаю его когда разраотаем контекстное меню. Сейчас создам новую ветку

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,5 +1,24 @@
-import {Module} from '../core/module'
+import { Module } from '../core/module';
 
 export class ClicksModule extends Module {
+  constructor(type, text) {
+    super(type, text);
+  }
 
+  trigger() {
+    let countClick = 0;
+    document.addEventListener('click', () => {
+      countClick++;
+    });
+    setInterval(() => {
+      alert(`Ну ты и накликал ${countClick}`);
+      document.removeEventListener('click', () => {
+        countClick++;
+      });
+    }, 3000);
+  }
+
+  toHTML() {
+    return super.toHTML();
+  }
 }
