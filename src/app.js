8d6fea9c5c8a6b920ba596b5c2adae58b3b09ebe
Dmitry Mamchenko <dmitry.mamchenko@gmail.com>
2023-05-13 15:23:51 +0300

    Перенес вызов меню из за дубля в методе open

--- src/app.js
+++ src/app.js
@@ -1,6 +1,17 @@
 import "./styles.css";
 import { ContextMenu } from "./menu";
 
+import { BackgroundModule } from '../src/modules/background.module.js';
+import { FigureModule } from '../src/modules/figure.module';
+import { ClicksModule } from '../src/modules/clicks.module';
+
+const moduleFeatches = [
+  new ClicksModule('clicks', 'Считать клики (за 3 секунды)'),
+  new BackgroundModule('background', 'Поменять цвет'),
+  new FigureModule('figure', 'Создать фигуру'),
+];
+
+
 const contextMenu = new ContextMenu("#menu");
 
 document.addEventListener("contextmenu", (event) => {
@@ -13,3 +24,8 @@ document.body.addEventListener('click',(event)=>{
 		contextMenu.close()
 	}
 })
+
+//перенес сюда. Чтобы не добавлялись повторно в меню.
+moduleFeatches.forEach(module => {
+  contextMenu.add(module);
+});
--- src/menu.js
+++ src/menu.js
@@ -1,17 +1,5 @@
 import { Menu } from "./core/menu";
-import { BackgroundModule } from '../src/modules/background.module.js';
-import { FigureModule } from '../src/modules/figure.module';
-import { ClicksModule } from '../src/modules/clicks.module';
-
-const moduleFeatches = [
-  new ClicksModule('clicks', 'Считать клики (за 3 секунды)'),
-  new BackgroundModule('background', 'Поменять цвет'),
-  new FigureModule('figure', 'Создать фигуру'),
-];
-
 export class ContextMenu extends Menu {
-  #moduleName
-
   constructor(selector) {
     super(selector);
   }
@@ -22,10 +10,6 @@ export class ContextMenu extends Menu {
 
     const windowWidth = document.documentElement.clientWidth;
     const windowHeight = document.documentElement.clientHeight;
-
-    moduleFeatches.forEach(module => {
-      this.add(module);
-    });
 
     if (positionX + this.el.offsetWidth >= windowWidth) {
       this.el.style.left = `${positionX - this.el.offsetWidth}px`;
@@ -41,21 +25,26 @@ export class ContextMenu extends Menu {
   }
 
   close() {
-	console.log('close');
-	this.el.classList.remove('open');
-	  document.querySelectorAll('li').forEach(elem => elem.remove());
+    console.log(this.el);
+    this.el.classList.remove('open');
   }
 
   add(module) {
+    console.log(module);
     const moduleHTML = module.toHTML(); //так как модуль возвращает HTML, создаем ноду
     this.el.insertAdjacentHTML('beforeend', moduleHTML); //добавление в пунт меню.
     let moduleNode = this.el.querySelector(`[data-type=${module.type}`);
     
     if (typeof moduleNode !== null) { //обработка клика
-      moduleNode.addEventListener('click', () => {
-        const nameModule = moduleNode.dataset.type;
-        const activeModule = moduleFeatches.find(modul => modul.type === nameModule);
+      moduleNode.addEventListener('click', (event) => {
+        // const nameModule = moduleNode.dataset.type;
+        // console.log(module);
+        // console.log(nameModule);
+        // // const activeModule = module.find(module => modul.type === nameModule);
+        const activeModule = module;
+
         activeModule.trigger();
+        this.close();
       });
     }
   }
