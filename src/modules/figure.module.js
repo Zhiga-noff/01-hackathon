1a3e863d258dbf4ee483d4e222ca572ef6881445
Dmitry Mamchenko <dmitry.mamchenko@gmail.com>
2023-05-14 12:54:09 +0300

    Добавлены комментарии к классу создания фигуры

--- src/modules/figure.module.js
+++ src/modules/figure.module.js
@@ -2,6 +2,7 @@ import { Module } from '../core/module';
 import { random } from '../utils';
 
 export class FigureModule extends Module {
+  //обявляем переменные которые нужны только в этом модуле
   #maxWidth;
   #minWidth;
   #maxHeight;
@@ -9,6 +10,7 @@ export class FigureModule extends Module {
 
   constructor(type, text) {
     super(type, text);
+    //устанавливаем ограничения по размерам
     this.#minWidth = 70;
     this.#maxWidth = 200;
     this.#minHeight = 70;
@@ -26,17 +28,23 @@ export class FigureModule extends Module {
     newFigure.style.width = `${width}px`;
     newFigure.style.height = `${height}px`;
     newFigure.style.borderRadius = `${borderRadius}%`;
+    //после того, как создали фигуру рандомно изменяем ее местоположение
     this.setPosition(newFigure, width, height);
     document.body.append(newFigure);
     this.remove(newFigure);
   }
 
+  /*
+  вспомогательная функция для создания фигуры.
+  Возвращает node
+  */
   create() {
     const figure = document.createElement('div');
     figure.classList.add('figure', 'animation', 'animation--show');
     return figure;
   }
 
+  //вспомогательный класс для удаления фигуры.
   remove(figure) {
     figure.classList.add('animation--hide');
 
@@ -46,8 +54,14 @@ export class FigureModule extends Module {
     }, 1500);
   }
 
+  /*
+  вспомогательная функция для установления рандомной позиции
+  принимает в себя node, и параметры высоты и ширины элемента
+  */
   setPosition(el, elWidth, elHeight) {
+    //получаем значения экрана, чтобы фигура за него не вызодила
     const position = document.body.getBoundingClientRect();
+    //устанавливаем дозволенную позицию. Окно минус фигура
     const posX = random(1, position.width - elWidth);
     const posY = random(1, position.height - elHeight);
     el.style.left = `${posX}px`;
